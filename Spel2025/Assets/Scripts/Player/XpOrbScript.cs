220aa3e21d1c7650d12a2c8d1ee2da8040654fe4
HelgeKv <124373018+HelgeKv@users.noreply.github.com>
2025-05-05 16:42:34 +0200

    XP ord falls now

--- Spel2025/Assets/Scripts/Player/XpOrbScript.cs
+++ Spel2025/Assets/Scripts/Player/XpOrbScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem.HID;
 
 public class XPOrb : MonoBehaviour
 {
@@ -8,9 +9,15 @@ public class XPOrb : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 2.0f;
     [SerializeField]
+    private float fallSpeed = 2.0f;
+    [SerializeField]
     private float maxSpeed = 10.0f;
     [SerializeField]
     private float acceleration = 3.0f;
+    [SerializeField]
+    private float fallAcceleration = 3.0f;
+    [SerializeField]
+    private LayerMask groundLayer;
 
     private Transform target;
     private bool isAttracted = false;
@@ -29,6 +36,25 @@ public class XPOrb : MonoBehaviour
 
             Vector3 direction = (target.position - transform.position).normalized;
             transform.position += direction * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            Fall();
+        }
+    }
+
+    private void Fall()
+    {
+        RaycastHit hit;
+        if (Physics.SphereCast(transform.position, 0.1f, -Vector3.up, out hit, 0f, groundLayer))
+        {
+            fallSpeed = 0f;
+            return;
+        }
+        else
+        {
+            fallSpeed = Mathf.Lerp(fallSpeed, 100f, fallAcceleration * Time.deltaTime);
+            transform.position += Vector3.down * fallSpeed * Time.deltaTime;
         }
     }
 
