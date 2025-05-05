d9e84f2ea787d5c52115081a53f934dc4437df44
luddewig <126206050+luddewig@users.noreply.github.com>
2025-05-05 18:32:49 +0200

    XP level progress

--- Spel2025/Assets/Scripts/Player/PlayerStats.cs
+++ Spel2025/Assets/Scripts/Player/PlayerStats.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -9,10 +10,16 @@ public class PlayerStats : MonoBehaviour
     private int level = 1;
     [SerializeField]
     private Slider experienceBar;
+    [SerializeField]
+    TextMeshProUGUI XPAmount;
+
 
     public void AddXP(int amount)
     {
         currentXP += amount;
+        experienceBar.value = currentXP;
+        //Hardcoded "out of 100" exp. Change here for leveling up.
+        XPAmount.text = currentXP.ToString() + " exp / 100 exp";
         Debug.Log("XP added: " + currentXP);
     }
 }
