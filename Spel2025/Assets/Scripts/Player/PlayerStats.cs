0c27301d3ad9dd8780399ec25d4b5c8410a9e61e
luddewig <126206050+luddewig@users.noreply.github.com>
2025-05-06 16:25:28 +0200

    levelup

--- Spel2025/Assets/Scripts/Player/PlayerStats.cs
+++ Spel2025/Assets/Scripts/Player/PlayerStats.cs
@@ -9,18 +9,43 @@ public class PlayerStats : MonoBehaviour
     [SerializeField]
     private int level = 1;
     [SerializeField]
+    private int xpToNextLevel = 100;
+    private int levelProgressXP = 0; // XP earned since last level-up
+    private int maxXP = 100;
+
+    //HUD components
+    [SerializeField]
     private Slider experienceBar;
     [SerializeField]
-    TextMeshProUGUI XPAmount;
+    TextMeshProUGUI XPAmountText;
+    [SerializeField]
+    TextMeshProUGUI levelText;
 
 
     public void AddXP(int amount)
     {
         currentXP += amount;
-        experienceBar.value = currentXP;
-        //Hardcoded "out of 100" exp. Change here for leveling up.
-        XPAmount.text = currentXP.ToString() + " exp / 100 exp";
-        Debug.Log("XP added: " + currentXP);
+        levelProgressXP += amount;
+
+        // Check for level up
+        while (levelProgressXP >= xpToNextLevel)
+        {
+            levelProgressXP -= xpToNextLevel;
+            level++;
+
+            // Increase XP requirement for next level (scaling)
+            xpToNextLevel += 50;
+
+            // Track the total XP needed to reach this level
+            maxXP += xpToNextLevel;
+
+            levelText.text = level.ToString();
+        }
+
+        experienceBar.maxValue = xpToNextLevel;
+        experienceBar.value = levelProgressXP;
+
+        XPAmountText.text = currentXP.ToString() + " exp / " + maxXP + " exp";
     }
 }
 
