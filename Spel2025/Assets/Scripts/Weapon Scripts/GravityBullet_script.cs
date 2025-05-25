bdbf9ba5d465d97a72531d9a8a24b116ffe35669
Gustav <112876953+ARadLad@users.noreply.github.com>
2025-05-25 21:21:37 +0200

    Balance tweaks

--- Spel2025/Assets/Scripts/Weapon Scripts/GravityBullet_script.cs
+++ Spel2025/Assets/Scripts/Weapon Scripts/GravityBullet_script.cs
@@ -11,8 +11,8 @@ public class GravityBullet_script : MonoBehaviour
     [SerializeField] private GameObject hit_particles;
 
     //Gravity bullet damage
-    [SerializeField] private static float damage = 5f;
-    [SerializeField] private static int maxLevel = 10;
+    [SerializeField] private static float damage = 8f;
+    [SerializeField] private static int maxLevel = 5;
     [SerializeField] private static int currentLevel = 0;
 
     // Start is called before the first frame update
@@ -41,7 +41,7 @@ public class GravityBullet_script : MonoBehaviour
     {
         if (currentLevel < maxLevel)
         {
-            damage += 2f;
+            damage += 3f;
             currentLevel++;
         }
         
--- Spel2025/Assets/Scripts/Weapon Scripts/Weapons_Explosion_script.cs
+++ Spel2025/Assets/Scripts/Weapon Scripts/Weapons_Explosion_script.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class Weapons_Explosion_script : MonoBehaviour
 {
     [SerializeField]
-    private static float max_damage = 20f;
+    private static float max_damage = 18f;
     [SerializeField]
-    private static float min_damage = 15f;
+    private static float min_damage = 12f;
     [SerializeField]
     private static float max_knockback = 20f;
     [SerializeField]
     private static float min_knockback = 10f;
     [SerializeField]
-    private static int maxLevel = 10;
+    private static int maxLevel = 5;
     [SerializeField]
     private static int currentLevel = 0;
 
@@ -46,8 +46,8 @@ public class Weapons_Explosion_script : MonoBehaviour
         if (currentLevel < maxLevel)
         {
             currentLevel++;
-            min_damage += 5f;
-            max_damage += 5f;
+            min_damage += 4f;
+            max_damage += 4f;
             min_knockback += 5f;
             max_knockback += 5f;
         }
@@ -56,8 +56,8 @@ public class Weapons_Explosion_script : MonoBehaviour
     public static void ResetUpgrade()
     {
         currentLevel = 0;
-        min_damage = 15f;
-        max_damage = 20f;
+        min_damage = 12f;
+        max_damage = 18f;
         min_knockback = 10f;
         max_knockback = 20f;
     }
